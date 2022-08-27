d74a5f1be90c36d88ee6811926cc2e75c5cbd909
java-saeng <gusdn85554@naver.com>
2022-08-28 02:41:09 +0900

    test : Acceptance Test, Integeration Test를 위해 Dummy Data 추가

---
+++ src/test/resources/sql/clean-up.sql
@@ -0,0 +1,2 @@
+delete from todo; alter table todo auto_increment = 1;
+delete from account; alter table account auto_increment = 1;
---
+++ src/test/resources/sql/data.sql
@@ -0,0 +1,7 @@
+insert into account (account_id) values (1);
+
+insert into todo (todo_id, created_at, updated_at, is_done, task, timer, account_id)
+values (1, '2021-08-27 09:00:00', '2021-08-27 09:00:00', 1, 'TDD 공부', '250', 1);
+
+insert into todo (todo_id, created_at, updated_at, is_done, task, timer, account_id)
+values (2, '2021-08-27 09:00:00', '2021-08-27 09:00:00', 1, 'ATDD 공부', '270', 1);
