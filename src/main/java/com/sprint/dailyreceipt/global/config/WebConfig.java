1456713e0f8ba213378a31c34a51ddd8e10224ad
java-saeng <gusdn85554@naver.com>
2022-09-13 22:37:22 +0900

    feat : login custom annotation 생성

--- src/main/java/com/sprint/dailyreceipt/global/config/WebConfig.java
+++ src/main/java/com/sprint/dailyreceipt/global/config/WebConfig.java
@@ -1,16 +1,29 @@
 package com.sprint.dailyreceipt.global.config;
 
+import com.sprint.dailyreceipt.global.annotation.LoginAccountArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginAccountArgumentResolver loginAccountArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginAccountArgumentResolver);
     }
 }
