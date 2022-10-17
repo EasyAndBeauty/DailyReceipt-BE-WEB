2d5612aa749779040a94949e9e4c00fc78d57df4
java-saeng <gusdn85554@naver.com>
2022-10-17 21:47:47 +0900

    fix : 계정 로그인 시 모든 todo 반환되는 버그 해결

--- src/main/java/com/sprint/dailyreceipt/domain/todo/dao/TodoRepositoryCustomImpl.java
+++ src/main/java/com/sprint/dailyreceipt/domain/todo/dao/TodoRepositoryCustomImpl.java
@@ -1,5 +1,6 @@
 package com.sprint.dailyreceipt.domain.todo.dao;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sprint.dailyreceipt.domain.todo.entity.Todo;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,11 @@ public class TodoRepositoryCustomImpl implements TodoRepositoryCustom{
     public List<Todo> findTodosByAccountId(long accountId) {
         return queryFactory.selectFrom(todo)
                            .innerJoin(todo.account, account).fetchJoin()
+                           .where(isSameAccount(accountId))
                            .fetch();
+    }
+
+    private BooleanExpression isSameAccount(long accountId) {
+        return todo.account.id.eq(accountId);
     }
 }
